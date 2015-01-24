956d6fa223bc4acb42c69b92f597d029347a762c
Jeff Barrera <jeff.barrera@stanford.edu>
2015-01-23 17:23:34 -0800

    Finished Death Row parsing assignment

--- homework/death-row-parsing/total-dr.sh
+++ homework/death-row-parsing/total-dr.sh
@@ -1,2 +1,32 @@
 # CA
-bash ca-dr.sh
+ca=$(bash ca-dr.sh)
+for LINE in $ca; do
+	echo "CA,$LINE" |\
+	sed 's/WHI/White/' |\
+	sed 's/BLA/Black/' |\
+	sed 's/HIS/Hispanic/' |\
+	sed 's/OTH/Other/'
+done
+
+# FL
+fl=$(bash fl-dr.sh)
+for LINE in $fl; do
+        echo "FL,$LINE" |\
+        sed 's/WM/White/' |\
+	sed 's/WF/White/' |\
+        sed 's/BM/Black/' |\
+	sed 's/BF/Black/' |\
+        sed 's/HM/Hispanic/' |\
+	sed 's/HF/Hispanic/' |\
+        sed 's/OM/Other/'
+done
+
+# TX
+tx=$(bash tx-dr.sh)
+for LINE in $tx; do
+        echo "TX,$LINE"
+done
+
+
+
+
--- homework/death-row-parsing/tx-dr.sh
+++ homework/death-row-parsing/tx-dr.sh
@@ -6,4 +6,5 @@ pup 'td:nth-of-type(7) {}'
 # open tx old
 cat data-hold/tx_old.html |\
 # get race for each offender
-pup 'td:nth-of-type(4) text{}'
+pup 'td:nth-of-type(4) text{}' |\
+sed 's/Native    American/Other/'
